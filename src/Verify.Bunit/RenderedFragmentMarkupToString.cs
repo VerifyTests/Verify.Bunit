9e187949ca3f2037342d7aa564d76d6081b2c756
Simon Cropp <simon.cropp@gmail.com>
2025-12-18 22:56:17 +1100

    refs

--- src/Verify.Bunit/RenderedFragmentMarkupToString.cs
+++ src/Verify.Bunit/RenderedFragmentMarkupToString.cs
@@ -2,8 +2,13 @@ static class RenderedFragmentMarkupToString
 {
     public static ConversionResult Convert(IRenderedComponent<IComponent> fragment, IReadOnlyDictionary<string, object> context)
     {
+        var formatter = new PrettyMarkupFormatter
+        {
+            Indentation = "  "
+        };
         var markup = fragment
-            .Nodes.ToHtml(new PrettyMarkupFormatter { Indentation = "  " })
+            .Nodes
+            .ToHtml(formatter)
             .Trim();
         return new(null, "html", markup);
     }
